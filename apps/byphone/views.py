2bca40ef2e8a8a4112f3719d42b80c2e1334cee8
Yaakov Azat <yaakovazat@gmail.com>
2019-02-23 21:04:06 +0600

    another test

--- apps/byphone/views.py
+++ apps/byphone/views.py
@@ -4,3 +4,20 @@ from azat import ROOT_DIR
 from datetime import datetime
 # Create your views here.
 def phone(request):
+    ddinfo = {}
+    if(request.method == 'POST'):
+        telephone = request.POST.get('telephone')
+        if (re.match(r'1[3,4,5,7,8]\d{9}', telephone)):
+            telNumStatus = '1'
+        else:
+            telNumStatus = '0'
+        (telephone.strip(' ')).strip('\n')
+        dt = datetime.now()
+        key = dt.strftime("%m%d%H%M%S")
+
+        if (telNumStatus == '1'):
+            context['value'] = key
+        else:
+            context['value'] = '手机号不合法!'
+
+    return render(request,'byphone.html',ddinfo)
