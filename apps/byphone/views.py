6688d82a8c38c8fa04ecd6e1791f96935c90d6e5
Yaakov Azat <yaakovazat@gmail.com>
2019-02-25 16:43:38 +0600

    Id

--- apps/byphone/views.py
+++ apps/byphone/views.py
@@ -30,7 +30,8 @@ def phone(request):
             for each in orders_in_db:
                 orders.append(each)
             for each in orders:
-                express_ID=each['express_id']
+                express_Id=float(each['express_id'])
+                express_Id = round(express_Id)
                 result = "\n订单编号:%s\n客户姓名:%s\n电话:%s\n快递公司:%s\n快递单号:%s\n订单状态:%s\n下单日期:%s\n审核日期:%s\n"%(each['ID'],each['order_name'],each['order_phone'],each['express'],express_ID,each['order_status'],each['order_get_time'],each['order_done_time'])
                 results.append(result)
             orders_info = results
